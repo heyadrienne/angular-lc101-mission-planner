096ca97ff4468cd433716d10d1f29a6f3b05f81e
Adrienne Meltzer <you@example.com>
2021-12-09 21:33:08 -0500

    Thru Bonus #1

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -18,4 +18,28 @@ export class CrewComponent implements OnInit {
   ngOnInit() {
   }
 
+  add(memberName, isFirst) {
+    if(this.crew.includes(memberName, isFirst)){
+      return;
+    } else{
+      this.crew.push({name: memberName, firstMission: isFirst});
+    }
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  memberBeingEdited: object = null;
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object) {
+    member['name'] = name;
+    this.memberBeingEdited = null;
+  }
+
 }
